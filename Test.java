069f0de7590f4416e7e72b01deb5e601ce19853c
laithshams <laithshams@users.noreply.github.com>
2019-10-28 13:25:40 +0200

    Test

---
+++ Test.java
@@ -0,0 +1,16 @@
+public class Test
+{
+	public static void main(String args[])
+	{
+		float n1=5;
+		float n2=10;
+		float sum=n1+n2;
+		float minus=n1-n2;
+		float multible=n1*n2;
+		float devide=n1/n2;
+		System.out.println(n1+"+"+n2+"="+sum);
+		System.out.println(n1+"-"+n2+"="+minus);
+		System.out.println(n1+"*"+n2+"="+multible);
+		System.out.println(n1+"/"+n2+"="+devide);
+	}
+}
